33a870b1315cca5594b388597953b4173186e1ac
ezequieljm <ezequieljm020@gmail.com>
2021-05-04 11:57:00 -0300

    Problema arreglado

--- src/components/Secretary/Secretary.jsx
+++ src/components/Secretary/Secretary.jsx
@@ -5,25 +5,76 @@ import Typography from '@material-ui/core/Typography';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import useStyles from './UseStyles/UseStyles';
 import arrIconsList from './Icons/ExportIcons';
 import AddBox from '@material-ui/icons/AddBox';
-
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button'
-
+import useStyles from './UseStyles/UseStyles'
 
 const txtTab = ['Clientes', 'Agenda', 'Solicitudes'];
+const configs = ['Configuración', 'Recargar página', 'Reportar problema', 'Consultar manual'];
 const requestsTool = ['Listar Solicitudes', 'Actualizar Lista', 'Imprimir', 'Herramientas'];
+
+function CreateMeeting() {
+  return (
+    <ListItem button key="crear-cita">
+      <ListItemIcon>
+        <AddBox />
+      </ListItemIcon>
+      <ListItemText primary="Crear Cita" />
+    </ListItem>
+  )
+}
+
+function RequestListTools({handleListItemClick, selectedIndex}) {
+  return(
+    <List>
+      <CreateMeeting />
+      {requestsTool.map((text, index) => (
+        <ListItem 
+          button 
+          key={text} 
+          selected={selectedIndex === index} 
+          onClick={e => handleListItemClick(e, index)}
+        >
+          <ListItemIcon>
+            {arrIconsList[index]}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  )
+}
+
+function PageTools({handleListItemClick, selectedIndex}) {
+  return (
+    <List>
+      {configs.map((text, index) => (
+        <ListItem 
+          button 
+          key={text}
+          selected={selectedIndex === index + 4}
+          onClick={e => handleListItemClick(e, index + 4)}
+        >
+          <ListItemIcon>{arrIconsList[index + 4]}</ListItemIcon>
+          <ListItemText primary={
+            <Typography variant="caption" display="block" gutterBottom>{text}</Typography>
+          }/>
+        </ListItem>
+      ))}
+    </List>
+  )
+}
 
 const Secretary = ({exit}) => {
   const classes = useStyles();
 
   /* State main content */ 
-  const [mainContent, setContent] = React.useState('');
+  const [mainContent, setContent] = React.useState('Solicitudes');
   const changeMainContent = aValue => setContent(txtTab[aValue]);
 
   /* This state is for the list items */
@@ -41,7 +92,6 @@ const Secretary = ({exit}) => {
 
   return (
     <div className={classes.root}>
-
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar className={classes.toolBar}>
           <Typography variant="h6" noWrap>
@@ -63,50 +113,12 @@ const Secretary = ({exit}) => {
           <h4>{getDate()}</h4>
         </div>
       </AppBar>
-
-
-      <Drawer className={classes.drawer} variant="permanent" classes={{ paper:classes.drawerPaper}}>
+      <Drawer className={classes.drawer} variant="permanent" classes={{paper:classes.drawerPaper}}>
         <Toolbar />
         <Toolbar />
         <div className={classes.drawerContainer}>
-          <List>
-            <ListItem button key="crear-cita" onClick={() => console.log('Crear cita')}>
-              <ListItemIcon>
-                <AddBox />
-              </ListItemIcon>
-              <ListItemText primary="Crear Cita" />
-            </ListItem>
-            {requestsTool.map((text, index) => (
-              <ListItem 
-                button 
-                key={text} 
-                selected={selectedIndex === index} 
-                onClick={e => handleListItemClick(e, index)}
-              >
-                <ListItemIcon>
-                  {arrIconsList[index]}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <List>
-            {['Configuración', 'Recargar página', 'Reportar problema', 'Consultar manual'].map((text, index) => (
-              <ListItem 
-                button 
-                key={text}
-                selected={selectedIndex === index + 4}
-                onClick={e => handleListItemClick(e, index + 4)}
-              >
-                <ListItemIcon>{arrIconsList[index + 4]}</ListItemIcon>
-                <ListItemText primary={
-                  <Typography variant="caption" display="block" gutterBottom className={classes.fontListText}>
-                    {text}
-                  </Typography>
-                }/>
-              </ListItem>
-            ))}
-          </List>
+          <RequestListTools handleListItemClick={handleListItemClick} selectedIndex={selectedIndex}/>
+          <PageTools handleListItemClick={handleListItemClick} selectedIndex={selectedIndex}/>
         </div>
       </Drawer>
       <main className={classes.content}>
