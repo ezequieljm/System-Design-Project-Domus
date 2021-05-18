0b15a4aa32c0712f224a5cf48c67abf12b683d9c
gab <gbangertes@gmail.com>
2021-05-17 23:02:56 -0300

    Terminado Jefa de Comercialización

--- src/components/BossOfCommerce/MainContent/Reports/RentalReports/RentalReports.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const RentalReports = () => {
-  return (
-    <h1>Reportes de Alquileres</h1>
-  )
-}
-
-export default RentalReports
--- src/components/BossOfCommerce/MainContent/Reports/SaleReports/SaleReports.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SaleReports = () => {
-  return (
-    <h1>Reportes de Ventas</h1>
-  )
-}
-
-export default SaleReports
